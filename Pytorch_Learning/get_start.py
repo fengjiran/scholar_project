2fcdce2046d1c62df7bbc4be270732373476c7f9
fengjiran <fengjiran@foxmail.com>
2018-03-23 11:28:30 +0800

    test pytorch

--- Pytorch_Learning/get_start.py
+++ Pytorch_Learning/get_start.py
@@ -4,9 +4,15 @@ import torch
 from torch.autograd import Variable
 
 x = Variable(torch.ones(2, 2), requires_grad=True)
+# x.requires_grad = True
 print(x)
 
 y = x + 2
-print(y)
+print(y.requires_grad)
 
-print(y.grad_fn)
+z = y * y * 3
+out = z.mean()
+out.backward()
+print(x.grad)
+
+a = Variable(torch.FloatTensor([2, 3]), requires_grad=True)
