66dea3654e4eecb26dd4924e334110356aeb8957
fengjiran <fengjiran@foxmail.com>
2018-03-25 17:26:18 +0800

    test

--- Pytorch_Learning/network.py
+++ Pytorch_Learning/network.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
+import torch.optim as optim
 
 
 class Net(nn.Module):
@@ -19,6 +20,10 @@ class Net(nn.Module):
         x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
         x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
         x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
     def num_flat_features(self, x):
         size = x.size()[1:]
@@ -26,3 +31,39 @@ class Net(nn.Module):
         for s in size:
             num_features *= s
         return num_features
+
+
+net = Net()
+# print(net)
+
+
+# params = list(net.parameters())
+# print(len(params))
+# print(params[0].size())
+# print(params[0].requires_grad)
+
+inpt = Variable(torch.randn(1, 1, 32, 32))
+out = net(inpt)
+# print(out)
+
+target = Variable(torch.arange(1, 11))
+criterion = nn.MSELoss()
+loss = criterion(out, target)
+print(loss)
+
+optimizer = optim.SGD(net.parameters(), lr=0.01)
+optimizer.zero_grad()
+
+
+net.zero_grad()
+print('conv1.bias.grad before backward')
+print(net.conv1.bias.grad)
+
+loss.backward()
+
+print('conv1.bias.grad after backward')
+print(net.conv1.bias.grad)
+
+lr = 0.01
+for f in net.parameters():
+    f.data.sub_(f.grad.data * lr)
