58f6afbd31a1a34085c1cea4f0d385a7a8e2a4a3
fengjiran <fengjiran@foxmail.com>
2018-03-27 09:56:46 +0800

    add dataset

--- Pytorch_Learning/dataset.py
+++ Pytorch_Learning/dataset.py
@@ -2,3 +2,11 @@ import torch
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
+
+
+def is_image_file(filename):
+    return any(filename.endswith(extension) for extension in [".png", ".jpg", ".jpeg"])
+
+
+class CelebaDataset(Dataset):
+    pass
