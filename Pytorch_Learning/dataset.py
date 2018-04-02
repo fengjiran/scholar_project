537c5f598b958b31bf727dd347973b29a6907a8f
fengjiran <fengjiran@foxmail.com>
2018-04-02 12:01:02 +0800

    dataset

--- Pytorch_Learning/dataset.py
+++ Pytorch_Learning/dataset.py
@@ -1,7 +1,16 @@
+import platform
+import pandas as pd
+import numpy as np
+
 import torch
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from torch.autograd import Variable
+
+if platform.system() == 'Windows':
+    compress_path = 'E:\\TensorFlow_Learning\\Inpainting\\GlobalLocalImageCompletion_TF\\CelebA\\celeba_train_path_win.pickle'
+elif platform.system() == 'Linux':
+    compress_path = '/home/richard/TensorFlow_Learning/Inpainting/GlobalLocalImageCompletion_TF/CelebA/celeba_train_path_linux.pickle'
 
 
 def is_image_file(filename):
@@ -9,4 +18,21 @@ def is_image_file(filename):
 
 
 class CelebaDataset(Dataset):
-    pass
+    """Construc celeba dataset."""
+
+    def __init__(self, image_filename_dir, input_transfrom=None):
+        super(CelebaDataset, self).__init__()
+        train_path = pd.read_pickle(image_filename_dir)
+        train_path.index = range(len(train_path))
+        train_path = train_path.ix[np.random.permutation(len(train_path))]
+        self.train_path = train_path[:]['image_path'].values.tolist()
+
+        self.input_transfrom = input_transfrom
+
+    def __getitem__(self, index):
+        """Get one item of dataset."""
+        pass
+
+    def __len__(self):
+        """Get the number of samples in dataset."""
+        return len(self.train_path)
