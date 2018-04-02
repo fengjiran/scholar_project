fcfdbfc15f4592562fb044643dffe2ae03f0b1be
fengjiran <fengjiran@foxmail.com>
2018-04-03 01:16:01 +0800

    test

---
+++ Pytorch_Learning/mnist.py
@@ -0,0 +1,104 @@
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms
+
+torch.manual_seed(1)
+
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(1)
+
+kwargs = {'num_workers': 2, 'pin_memory': True} if torch.cuda.is_available() else {}
+
+tsf = transforms.Compose([transforms.ToTensor(),
+                          transforms.Normalize((0.1307,), (0.3081,))])
+
+
+train_dataset = datasets.MNIST('/home/richard/datasets/',
+                               train=True,
+                               download=False,
+                               transform=tsf)
+
+test_dataset = datasets.MNIST('/home/richard/datasets/',
+                              train=False,
+                              transform=tsf)
+
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, **kwargs)
+test_loader = DataLoader(test_dataset, batch_size=1000, shuffle=True, **kwargs)
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=10, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=10, out_channels=20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(in_features=320, out_features=50)
+        self.fc2 = nn.Linear(in_features=50, out_features=10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+
+model = Net()
+if torch.cuda.is_available():
+    model.cuda()
+
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+
+
+def train(epoch):
+    model.train()
+    batch_idx = 0
+    for data, target in train_loader:
+        if torch.cuda.is_available():
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data), Variable(target)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+
+        if batch_idx % 10 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.data[0]
+            ))
+        batch_idx += 1
+
+
+def test():
+    model.eval()
+    test_loss = 0
+    correct = 0
+    for data, target in test_loader:
+        if torch.cuda.is_available():
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data, volatile=True), Variable(target)
+        output = model(data)
+        test_loss += F.nll_loss(output, target, size_average=False).data[0]
+        pred = output.data.max(1, keepdim=True)[1]
+        correct += pred.eq(target.data.view_as(pred)).long().cpu().sum()
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
+for epoch in range(1, 10 + 1):
+    train(epoch)
+    test()
