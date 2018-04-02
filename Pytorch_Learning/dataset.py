a9eec7650e7212f88b6abd5925902dc15459601f
fengjiran <fengjiran@foxmail.com>
2018-04-02 20:16:05 +0800

    input transfors

--- Pytorch_Learning/dataset.py
+++ Pytorch_Learning/dataset.py
@@ -21,7 +21,45 @@ def is_image_file(filename):
 
 
 def input_transfrom(image_path):
-    img = Image.open(image_path)
+    low = 48
+    high = 96
+    image_height = 178
+    image_width = 178
+    gt_height = 96
+    gt_width = 96
+
+    img = Image.open(image_path).convert('RGB')
+    img.show()
+    ori_image = transforms.RandomCrop((image_height, image_width))(img)  # range [0, 255]
+
+    hole_height, hole_width = np.random.randint(low, high, size=(2))
+    y = np.random.randint(0, image_height - hole_height)
+    x = np.random.randint(0, image_width - hole_width)
+
+    mask = np.ones((hole_height, hole_width), dtype=np.float32)
+    mask = np.pad(mask,
+                  ((y, image_height - hole_height - y), (x, image_width - hole_width - x)),
+                  'constant')
+    mask = np.reshape(mask, [image_height, image_width, 1])
+    mask = np.concatenate([mask] * 3, 2)
+
+    x_loc = np.random.randint(low=np.max([0, x + hole_width - gt_width]),
+                              high=np.min([x, image_width - gt_width]) + 1)
+    y_loc = np.random.randint(low=np.max([0, y + hole_height - gt_height]),
+                              high=np.min([y, image_height - gt_height]) + 1)
+
+    image_with_hole = np.array(ori_image) * (1 - mask) + mask * 255.  # range [0,255]
+
+    tsf = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+    ])
+
+    ori_image = tsf(ori_image)  # range [-1,1]
+    image_with_hole = tsf(image_with_hole)  # range [-1, 1]
+    mask = transforms.ToTensor()(mask * 255)
+
+    return ori_image, image_with_hole, mask, x_loc, y_loc
 
 
 class CelebaDataset(Dataset):
@@ -43,3 +81,20 @@ class CelebaDataset(Dataset):
     def __len__(self):
         """Get the number of samples in dataset."""
         return len(self.train_path)
+
+
+if __name__ == '__main__':
+    img_path = '/Users/apple/Desktop/richard/Tensorflow_Learning/Inpainting/GlobalLocalImageCompletion_TF/CelebA/000013.png'
+    ori_image, image_with_hole, mask, x_loc, y_loc = input_transfrom(img_path)
+
+    print(ori_image.size())
+    print(image_with_hole.size())
+    print(mask.size())
+    print(x_loc, y_loc)
+
+    # ori_image = transforms.ToPILImage()(ori_image)
+    # image_with_hole = transforms.ToPILImage()(image_with_hole)
+    # mask = transforms.ToPILImage()(mask)
+    # ori_image.show()
+    # image_with_hole.show()
+    # mask.show()
